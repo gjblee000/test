cdead80a5346294b9751b6dd93131b82810b7a1e
Joony Lee <gjblee000@gmail.com>
2020-05-14 15:38:43 -0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-mkdir -p ~/download
-cd ~/download
-wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo apt-get update
+apt install docker.io
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
