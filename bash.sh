3102bcde6889befde390510cec5f3b0abdaddd66
Joony Lee <gjblee000@gmail.com>
2020-05-14 14:12:45 -0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -3,6 +3,3 @@
 mkdir -p ~/download
 cd ~/download
 wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -ivh epel-release-latest-7.noarch.rpm
-yum -y install cowsay
-cowsay ExaGridDba
